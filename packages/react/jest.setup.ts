767534e081c8e31e125b214927967cde33786f11
Ashley Ryan <asryan@vmware.com>
2022-03-10 16:15:05 -0500

    test(react): upgrade to react 17 and remove enzyme - add react-testing-library - upgrade jest/jsdom dependencies
    Signed-off-by: Ashley Ryan <asryan@vmware.com>

--- packages/react/jest.setup.ts
+++ packages/react/jest.setup.ts
@@ -1,4 +1,26 @@
-import * as enzyme from 'enzyme';
-import * as Adapter from 'enzyme-adapter-react-16';
+import '@testing-library/jest-dom/extend-expect';
 
-enzyme.configure({ adapter: new Adapter() });
+// jsdom doesn't implment IntersectionObserver
+// https://github.com/jsdom/jsdom/issues/2032
+if (typeof window !== 'undefined') {
+  const mockObserverAPI = () =>
+    jest.fn().mockReturnValue({
+      observe: jest.fn(),
+      unobserve: jest.fn(),
+      disconnect: jest.fn(),
+    });
+
+  window.ResizeObserver = mockObserverAPI();
+  window.IntersectionObserver = mockObserverAPI();
+
+  // jest implements getBoundingClientRect, but not DOMRect
+  if (!window.DOMRect) {
+    const domRectMock = () =>
+      jest.fn().mockReturnValue({
+        fromRect: jest.fn(),
+      });
+
+    // @ts-ignore
+    window.DOMRect = domRectMock();
+  }
+}
