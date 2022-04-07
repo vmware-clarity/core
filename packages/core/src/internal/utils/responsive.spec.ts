80b80c494a349d85e32c45d9a4674790da2c5d4a
Steve Haar <info@stevehaar.com>
2022-04-07 15:45:36 -0500

    chore: remove broken tests

--- packages/core/src/internal/utils/responsive.spec.ts
+++ packages/core/src/internal/utils/responsive.spec.ts
@@ -5,45 +5,10 @@
  */
 
 import { html } from 'lit';
-import { createTestElement, removeTestElement, componentIsStable } from '@cds/core/test';
-import { CdsControl } from '@cds/core/forms/index.js';
+import { createTestElement, removeTestElement } from '@cds/core/test';
 import { elementVisible } from './responsive.js';
 import '@cds/core/forms/register.js';
 import '@cds/core/button-action/register.js';
-
-describe('responsive utilities', () => {
-  let element: HTMLElement;
-  let component: CdsControl;
-
-  beforeEach(async () => {
-    element = await createTestElement(html`
-      <cds-control layout="horizontal">
-        <label>test</label>
-        <input type="text" />
-        <cds-control-message>message text</cds-control-message>
-      </cds-control>
-    `);
-
-    component = element.querySelector<CdsControl>('cds-control');
-  });
-
-  afterEach(() => {
-    removeTestElement(element);
-  });
-
-  xit('should get optimal component layout', async () => {
-    await componentIsStable(component);
-    expect(component.layout).toBe('horizontal');
-
-    component.style.width = '100px';
-
-    // hacky workaround as we cant trigger resize observers manually in headless browsers
-    (component as any).observers.filter((o: any) => o.__testTrigger).forEach((o: any) => o.__testTrigger());
-
-    await componentIsStable(component);
-    expect(component.layout).toBe('vertical');
-  });
-});
 
 describe('visibility utilities', () => {
   let element: HTMLElement;
