f5dabf0c3c9b4cbaf85a36030611c9f5c82699e7
Steve Haar <info@stevehaar.com>
2022-04-07 15:45:35 -0500

    chore: remove apps

--- packages/core/build/parse/website-demos.ts
+++ packages/core/build/parse/website-demos.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2020 VMware, Inc.
+ * Copyright (c) 2016-2022 VMware, Inc.
  * All Rights ReserveThis software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -20,13 +20,3 @@ fileArray.forEach(file => {
   };
   stories.push(docDemos);
 });
-
-// Note: even though this is in the parse directory, it executes the scripts form the build directory, one level up from here.const saveDirectory
-const progress: boolean = saveCoreDemos(stories, '../../../apps/website/data/core-demos.json'); // file path is hard coded in this utility
-
-if (progress) {
-  console.log(`Core demo data generated in website data folder.`);
-  process.exit(0);
-} else {
-  process.exit(1);
-}
