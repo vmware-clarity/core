1e30c78e42750995b934fae905e28571f0fd2447
Andrea Fernandes <andreaf1@vmware.com>
2024-08-06 11:45:16 +0530

    fix(vertical-nav): update performance tests

--- projects/core/src/index.performance.ts
+++ projects/core/src/index.performance.ts
@@ -9,13 +9,13 @@ import { testBundleSize } from 'web-test-runner-performance/browser.js';
 describe('performance', () => {
   it(`should meet maximum individual css bundle size limits`, async () => {
     expect((await testBundleSize('@cds/core/global.min.css')).kb).toBeLessThan(9.4);
-    expect((await testBundleSize('@cds/core/styles/theme.dark.min.css')).kb).toBeLessThan(1.236);
+    expect((await testBundleSize('@cds/core/styles/theme.dark.min.css')).kb).toBeLessThan(1.26);
     expect((await testBundleSize('@cds/core/list/list.min.css')).kb).toBeLessThan(0.5);
 
     // contained in @cds/core/global.min.css
     expect((await testBundleSize('@cds/core/styles/module.layout.min.css')).kb).toBeLessThan(4.6);
     expect((await testBundleSize('@cds/core/styles/module.reset.min.css')).kb).toBeLessThan(0.5);
-    expect((await testBundleSize('@cds/core/styles/module.tokens.min.css')).kb).toBeLessThan(3.62);
+    expect((await testBundleSize('@cds/core/styles/module.tokens.min.css')).kb).toBeLessThan(3.663);
     expect((await testBundleSize('@cds/core/styles/module.typography.min.css')).kb).toBeLessThan(1.616);
   });
 
