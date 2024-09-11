2166bf222cc60ce58904cbbb085f90c92be29622
mivaylo <mivaylo@vmware.com>
2024-09-11 14:15:09 +0300

    feat(icons): format story

--- projects/core/src/icon/icon.stories.ts
+++ projects/core/src/icon/icon.stories.ts
@@ -184,8 +184,9 @@ export function API(args: any) {
 
 export function iconsV3_1_0() {
   return (
-    html`<h3>Charts</h3>` +
+    html`<b>Charts</b>` +
     `Axis-chart (Dev) Axes (Figma):` +
+    `<br/>` +
     `<cds-icon shape="axis-chart" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="axis-chart" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="axis-chart" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
@@ -194,6 +195,7 @@ export function iconsV3_1_0() {
     `<cds-icon shape="axis-chart" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<br/>` +
     `Bar-chart (Dev) Bar (Figma):` +
+    `<br/>` +
     `<cds-icon shape="bar-chart" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="bar-chart" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="bar-chart" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
@@ -202,6 +204,7 @@ export function iconsV3_1_0() {
     `<cds-icon shape="bar-chart" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<br/>` +
     `box-plot (Dev) box (Figma):` +
+    `<br/>` +
     `<cds-icon shape="box-plot" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="box-plot" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="box-plot" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
@@ -210,6 +213,7 @@ export function iconsV3_1_0() {
     `<cds-icon shape="box-plot" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<br/>` +
     `bubble-chart (Dev) bubble (Figma):` +
+    `<br/>` +
     `<cds-icon shape="bubble-chart" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="bubble-chart" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="bubble-chart" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
@@ -218,6 +222,7 @@ export function iconsV3_1_0() {
     `<cds-icon shape="bubble-chart" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<br/>` +
     `cloud (Dev) cloud (Figma):` +
+    `<br/>` +
     `<cds-icon shape="cloud" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="cloud" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="cloud" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
@@ -226,6 +231,7 @@ export function iconsV3_1_0() {
     `<cds-icon shape="cloud" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<br/>` +
     `curve-chart (Dev) curve (Figma):` +
+    `<br/>` +
     `<cds-icon shape="curve-chart" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="curve-chart" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="curve-chart" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
@@ -234,6 +240,7 @@ export function iconsV3_1_0() {
     `<cds-icon shape="curve-chart" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<br/>` +
     `grid-chart (Dev) grid (Figma):` +
+    `<br/>` +
     `<cds-icon shape="grid-chart" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="grid-chart" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="grid-chart" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
@@ -242,6 +249,7 @@ export function iconsV3_1_0() {
     `<cds-icon shape="grid-chart" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<br/>` +
     `heat-map (Dev) heat (Figma):` +
+    `<br/>` +
     `<cds-icon shape="heat-map" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="heat-map" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="heat-map" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
@@ -250,6 +258,7 @@ export function iconsV3_1_0() {
     `<cds-icon shape="heat-map" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<br/>` +
     `line-chart (Dev) line (Figma):` +
+    `<br/>` +
     `<cds-icon shape="line-chart" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="line-chart" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="line-chart" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
@@ -258,6 +267,7 @@ export function iconsV3_1_0() {
     `<cds-icon shape="line-chart" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<br/>` +
     `pie-chart (Dev) pie (Figma):` +
+    `<br/>` +
     `<cds-icon shape="pie-chart" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="pie-chart" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="pie-chart" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
@@ -266,6 +276,7 @@ export function iconsV3_1_0() {
     `<cds-icon shape="pie-chart" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<br/>` +
     `scatter-plot (Dev) scatter (Figma):` +
+    `<br/>` +
     `<cds-icon shape="scatter-plot" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="scatter-plot" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="scatter-plot" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
@@ -274,6 +285,7 @@ export function iconsV3_1_0() {
     `<cds-icon shape="scatter-plot" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<br/>` +
     `tick-chart (Dev) ticks (Figma):` +
+    `<br/>` +
     `<cds-icon shape="tick-chart" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="tick-chart" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="tick-chart" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
@@ -282,6 +294,7 @@ export function iconsV3_1_0() {
     `<cds-icon shape="tick-chart" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<br/>` +
     `bell-curve (Dev) bell-curve (Figma):` +
+    `<br/>` +
     `<cds-icon shape="bell-curve" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="bell-curve" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="bell-curve" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
