96e0b018b36f96da3f035f7126bdfce3c10379a3
mivaylo <mivaylo@vmware.com>
2024-09-12 15:15:01 +0300

    feat(icons): add media icons in story

--- projects/core/src/icon/icon.stories.ts
+++ projects/core/src/icon/icon.stories.ts
@@ -47,6 +47,27 @@ import { rubleIcon } from '@cds/core/icon/shapes/ruble.js';
 import { employeeIcon } from '@cds/core/icon/shapes/employee.js';
 import { employeeGroupIcon } from '@cds/core/icon/shapes/employee-group.js';
 import { factoryIcon } from '@cds/core/icon/shapes/factory.js';
+import { playIcon } from '@cds/core/icon/shapes/play.js';
+import { pauseIcon } from '@cds/core/icon/shapes/pause.js';
+import { stepForwardIcon } from '@cds/core/icon/shapes/step-forward.js';
+import { stopIcon } from '@cds/core/icon/shapes/stop.js';
+import { powerIcon } from '@cds/core/icon/shapes/power.js';
+import { rewindIcon } from '@cds/core/icon/shapes/rewind.js';
+import { fastForwardIcon } from '@cds/core/icon/shapes/fast-forward.js';
+import { cameraIcon } from '@cds/core/icon/shapes/camera.js';
+import { videoCameraIcon } from '@cds/core/icon/shapes/video-camera.js';
+import { volumeUpIcon } from '@cds/core/icon/shapes/volume-up.js';
+import { volumeDownIcon } from '@cds/core/icon/shapes/volume-down.js';
+import { volumeMuteIcon } from '@cds/core/icon/shapes/volume-mute.js';
+import { headphonesIcon } from '@cds/core/icon/shapes/headphones.js';
+import { filmStripIcon } from '@cds/core/icon/shapes/film-strip.js';
+import { musicNoteIcon } from '@cds/core/icon/shapes/music-note.js';
+import { imageGalleryIcon } from '@cds/core/icon/shapes/image-gallery.js';
+import { videoGalleryIcon } from '@cds/core/icon/shapes/video-gallery.js';
+import { shuffleIcon } from '@cds/core/icon/shapes/shuffle.js';
+import { wandIcon } from '@cds/core/icon/shapes/wand.js';
+
+
 
 import { html, LitElement } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
@@ -95,7 +116,28 @@ ClarityIcons.addIcons(
   rubleIcon,
   employeeIcon,
   employeeGroupIcon,
-  factoryIcon
+  factoryIcon,
+
+  // Media
+  playIcon,
+  pauseIcon,
+  stepForwardIcon,
+  stopIcon,
+  powerIcon,
+  rewindIcon,
+  fastForwardIcon,
+  cameraIcon,
+  videoCameraIcon,
+  volumeUpIcon,
+  volumeDownIcon,
+  volumeMuteIcon,
+  headphonesIcon,
+  filmStripIcon,
+  musicNoteIcon,
+  imageGalleryIcon,
+  videoGalleryIcon,
+  shuffleIcon,
+  wandIcon
 );
 
 export default {
@@ -559,7 +601,181 @@ export function iconsV3_1_0() {
     `<cds-icon shape="factory" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="factory" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="factory" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
-    `<cds-icon shape="factory" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>`
+    `<cds-icon shape="factory" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `Charts` +
+    `</br>` +
+    `</br>` +
+    `<br/>` +
+    `play (Dev) play (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="play" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="play" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="play" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="play" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="play" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="play" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `pause (Dev) pause (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="pause" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="pause" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="pause" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="pause" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="pause" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="pause" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `step-forward (Dev) step-forward (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="step-forward" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="step-forward" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="step-forward" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="step-forward" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="step-forward" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="step-forward" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `stop (Dev) stop (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="stop" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="stop" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="stop" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="stop" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="stop" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="stop" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `power (Dev) power (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="power" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="power" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="power" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="power" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="power" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="power" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `rewind (Dev) rewind (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="rewind" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="rewind" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="rewind" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="rewind" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="rewind" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="rewind" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `fast-forward (Dev) fast-forward (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="fast-forward" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="fast-forward" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="fast-forward" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="fast-forward" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="fast-forward" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="fast-forward" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `camera (Dev) camera (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="camera" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="camera" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="camera" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="camera" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="camera" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="camera" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `video-camera (Dev) video-camera (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="video-camera" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="video-camera" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="video-camera" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="video-camera" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="video-camera" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="video-camera" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `volume-up (Dev) volume-up (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="volume-up" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="volume-up" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="volume-up" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="volume-up" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="volume-up" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="volume-up" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `volume-down (Dev) volume-down (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="volume-down" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="volume-down" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="volume-down" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="volume-down" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="volume-down" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="volume-down" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `volume-mute (Dev) volume-mute (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="volume-mute" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="volume-mute" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="volume-mute" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="volume-mute" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="volume-mute" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="volume-mute" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `headphones (Dev) headphones (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="headphones" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="headphones" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="headphones" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="headphones" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="headphones" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="headphones" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `film-strip (Dev) film-strip (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="film-strip" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="film-strip" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="film-strip" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="film-strip" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="film-strip" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="film-strip" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `music-note (Dev) music-note (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="music-note" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="music-note" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="music-note" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="music-note" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="music-note" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="music-note" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `image-gallery (Dev) image-gallery (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="image-gallery" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="image-gallery" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="image-gallery" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="image-gallery" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="image-gallery" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="image-gallery" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `video-gallery (Dev) video-gallery (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="video-gallery" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="video-gallery" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="video-gallery" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="video-gallery" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="video-gallery" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="video-gallery" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `shuffle (Dev) shuffle (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="shuffle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="shuffle" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="shuffle" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="shuffle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="shuffle" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="shuffle" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `wand (Dev) wand (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="wand" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="wand" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="wand" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="wand" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="wand" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="wand" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>`
   );
 }
 
