6b02151a3d3d6593b592cfceac61080da5397654
mivaylo <mivaylo@vmware.com>
2024-09-12 10:35:29 +0300

    feat(icons): add commerce in story

--- projects/core/src/icon/icon.stories.ts
+++ projects/core/src/icon/icon.stories.ts
@@ -25,6 +25,28 @@ import { tickChartIcon } from '@cds/core/icon/shapes/tick-chart.js';
 import { bellCurveIcon } from '@cds/core/icon/shapes/bell-curve.js';
 
 import { calculatorIcon } from '@cds/core/icon/shapes/calculator.js';
+import { piggyBankIcon } from '@cds/core/icon/shapes/piggy-bank.js';
+import { shoppingBagIcon } from '@cds/core/icon/shapes/shopping-bag.js';
+import { shoppingCartIcon } from '@cds/core/icon/shapes/shopping-cart.js';
+import { walletIcon } from '@cds/core/icon/shapes/wallet.js';
+import { storeIcon } from '@cds/core/icon/shapes/store.js';
+import { euroIcon } from '@cds/core/icon/shapes/euro.js';
+import { dollarIcon } from '@cds/core/icon/shapes/dollar.js';
+import { creditCardIcon } from '@cds/core/icon/shapes/credit-card.js';
+import { bankIcon } from '@cds/core/icon/shapes/bank.js';
+import { dollarBillIcon } from '@cds/core/icon/shapes/dollar-bill.js';
+import { eCheckIcon } from '@cds/core/icon/shapes/e-check.js';
+import { poundIcon } from '@cds/core/icon/shapes/pound.js';
+import { rupeeIcon } from '@cds/core/icon/shapes/rupee.js';
+import { wonIcon } from '@cds/core/icon/shapes/won.js';
+import { yenIcon } from '@cds/core/icon/shapes/yen.js';
+import { bitcoinIcon } from '@cds/core/icon/shapes/bitcoin.js';
+import { pesoIcon } from '@cds/core/icon/shapes/peso.js';
+import { coinBagIcon } from '@cds/core/icon/shapes/coin-bag.js';
+import { rubleIcon } from '@cds/core/icon/shapes/ruble.js';
+import { employeeIcon } from '@cds/core/icon/shapes/employee.js';
+import { employeeGroupIcon } from '@cds/core/icon/shapes/employee-group.js';
+import { factoryIcon } from '@cds/core/icon/shapes/factory.js';
 
 import { html, LitElement } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
@@ -51,7 +73,29 @@ ClarityIcons.addIcons(
   tickChartIcon,
   bellCurveIcon,
   // Commerce
-  calculatorIcon
+  calculatorIcon,
+  piggyBankIcon,
+  shoppingBagIcon,
+  shoppingCartIcon,
+  walletIcon,
+  storeIcon,
+  euroIcon,
+  dollarIcon,
+  creditCardIcon,
+  bankIcon,
+  dollarBillIcon,
+  eCheckIcon,
+  poundIcon,
+  rupeeIcon,
+  wonIcon,
+  yenIcon,
+  bitcoinIcon,
+  pesoIcon,
+  coinBagIcon,
+  rubleIcon,
+  employeeIcon,
+  employeeGroupIcon,
+  factoryIcon
 );
 
 export default {
@@ -317,7 +361,205 @@ export function iconsV3_1_0() {
     `<cds-icon shape="calculator" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="calculator" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
     `<cds-icon shape="calculator" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
-    `<cds-icon shape="calculator" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>`
+    `<cds-icon shape="calculator" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `piggy-bank (Dev) piggy-bank (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="piggy-bank" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="piggy-bank" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="piggy-bank" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="piggy-bank" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="piggy-bank" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="piggy-bank" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `shopping-bag (Dev) shopping-bag (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="shopping-bag" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="shopping-bag" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="shopping-bag" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="shopping-bag" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="shopping-bag" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="shopping-bag" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `shopping-cart (Dev) shopping-cart (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="shopping-cart" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="shopping-cart" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="shopping-cart" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="shopping-cart" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="shopping-cart" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="shopping-cart" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `wallet (Dev) wallet (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="wallet" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="wallet" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="wallet" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="wallet" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="wallet" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="wallet" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `store (Dev) store (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="store" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="store" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="store" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="store" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="store" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="store" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `euro (Dev) euro (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="euro" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="euro" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="euro" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="euro" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="euro" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="euro" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `dollar (Dev) dollar (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="dollar" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="dollar" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="dollar" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="dollar" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="dollar" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="dollar" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `credit-card (Dev) credit-card (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="credit-card" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="credit-card" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="credit-card" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="credit-card" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="credit-card" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="credit-card" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `bank (Dev) bank (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="bank" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="bank" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="bank" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="bank" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="bank" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="bank" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `dollar-bill (Dev) dollar-bill (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="dollar-bill" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="dollar-bill" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="dollar-bill" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="dollar-bill" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="dollar-bill" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="dollar-bill" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `e-check (Dev) e-check (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="e-check" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="e-check" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="e-check" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="e-check" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="e-check" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="e-check" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `pound (Dev) pound (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="pound" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="pound" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="pound" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="pound" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="pound" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="pound" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `rupee (Dev) rupee (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="rupee" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="rupee" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="rupee" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="rupee" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="rupee" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="rupee" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `won (Dev) won (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="won" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="won" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="won" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="won" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="won" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="won" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `yen (Dev) yen (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="yen" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="yen" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="yen" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="yen" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="yen" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="yen" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `bitcoin (Dev) bitcoin (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="bitcoin" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="bitcoin" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="bitcoin" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="bitcoin" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="bitcoin" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="bitcoin" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `peso (Dev) peso (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="peso" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="peso" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="peso" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="peso" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="peso" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="peso" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `coin-bag (Dev) coin-bag (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="coin-bag" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="coin-bag" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="coin-bag" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="coin-bag" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="coin-bag" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="coin-bag" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `ruble (Dev) ruble (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="ruble" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="ruble" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="ruble" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="ruble" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="ruble" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="ruble" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `employee (Dev) employee (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="employee" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="employee" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="employee" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="employee" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="employee" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="employee" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `employee-grou (Dev) employee-grou (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="employee-grou" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="employee-grou" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="employee-grou" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="employee-grou" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="employee-grou" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="employee-grou" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<br/>` +
+    `factory (Dev) factory (Figma):` +
+    `<br/>` +
+    `<cds-icon shape="factory" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="factory" badge="warning-triangle" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="factory" badge="danger" role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="factory" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="factory" badge="warning-triangle" solid role="img" aria-label="Icons v3.1.0"></cds-icon>` +
+    `<cds-icon shape="factory" badge="danger" solid role="img" aria-label="Icons v3.1.0"></cds-icon>`
   );
 }
 
