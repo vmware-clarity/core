cf564b7bfdfc18608f65cba2b2429e66d8054136
mivaylo <132376042+mivaylo@users.noreply.github.com>
2024-09-25 16:34:10 +0300

    feat(icons): implement changes from v3.1.0 (#323)
    ## PR Checklist
    
    Please check if your PR fulfills the following requirements:
    
    - [ ] Tests for the changes have been added (for bug fixes / features)
    - [ ] Docs have been added / updated (for bug fixes / features)
    - [ ] If applicable, have a visual design approval
    
    ## PR Type
    
    What kind of change does this PR introduce?
    
    <!-- Please check the one that applies to this PR using "x". -->
    
    - [ ] Bugfix
    - [x] Feature
    - [ ] Code style update (formatting, local variables)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] CI related changes
    - [ ] Documentation content changes
    - [ ] clarity.design website / infrastructure changes
    - [ ] Other... Please describe:
    
    ## What is the current behavior?
    
    <!-- Please describe the current behavior that you are modifying, or
    link to a relevant issue. -->
    
    Issue Number: CDE-1985
    
    ## What is the new behavior?
    
    ## Does this PR introduce a breaking change?
    
    - [x] Yes
    - [ ] No
    
    <!-- If this PR contains a breaking change, please describe the impact
    and migration path for existing applications below. -->
    
    ## Other information

--- projects/core/src/icon/shapes/window-restore.ts
+++ projects/core/src/icon/shapes/window-restore.ts
@@ -9,7 +9,7 @@ import { IconShapeTuple } from '../interfaces/icon.interfaces.js';
 
 const icon = {
   outline:
-    '<path d="M28,8H14a2,2,0,0,0-2,2v2h2V10H28V20H26v2h2a2,2,0,0,0,2-2V10A2,2,0,0,0,28,8Z"/><path d="M22,14H8a2,2,0,0,0-2,2V26a2,2,0,0,0,2,2H22a2,2,0,0,0,2-2V16A2,2,0,0,0,22,14ZM8,26V16H22V26Z"/>',
+    '<path d="M14 8H28C29.1046 8 30 8.89543 30 10V20C30 21.1046 29.1046 22 28 22H26V20H28V10H14V12H12V10C12 8.89543 12.8954 8 14 8Z"/><path fill-rule="evenodd" clip-rule="evenodd" d="M22 14H8C6.89543 14 6 14.8954 6 16V26C6 27.1046 6.89543 28 8 28H22C23.1046 28 24 27.1046 24 26V16C24 14.8954 23.1046 14 22 14ZM8 26V16H22V26H8Z"/>',
 };
 
 export const windowRestoreIconName = 'window-restore';
